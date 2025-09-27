7299af52b045ba2151519cdd0702677f8ec83e57
Akeboshiii <johnkuraya491@gmail.com>
2025-09-28 03:08:05 +0800

    feat: set up basic Express server and add initial HTML structure

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import url from 'url'
+import path from 'path'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const PORT = 3000
+
+app.use('/', express.static(path.resolve(__dirname, 'public')))
 
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
